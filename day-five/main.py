07b664fb111742e356ab9d1928ff076d51705d8f
mambo-dev <mambo.michael.22@gmail.com>
2023-11-30 06:21:10 +0300

    complete execute instructions function

--- day-five/main.py
+++ day-five/main.py
@@ -19,19 +19,29 @@ def split_crates_and_instructions(file_contents):
 def transform_crates(crates):
     
     crate_lines = crates.split("\n")
-    final_output = {}
+    positional_output = {}
     value_positions  = []
     for lines in crate_lines:
         for i in range(0, len(lines)):
             if lines[i] != " " and lines[i] != "[" and lines[i] != "]":
-                final_output[i] = []
+                positional_output[i] = []
                 value_positions.append((lines[i], i))
      
    
     for value in value_positions:
-        for output in final_output:
+        for output in positional_output:
             if value[1] == output:
-                final_output[output].append(value[0])
+                positional_output[output].append(value[0])
+    final_output = {}
+    for output in positional_output:
+        output_entry = positional_output[output]
+        final_output[int(output_entry[-1])] =  output_entry[0:-1]
+    
+    for final in final_output:
+        final_output[final].reverse()
+        
+
+
 
     return final_output
 
@@ -55,11 +65,28 @@ def transform_instructions(instructions):
 
 
 def execute_instructions(instructions,  crates):
-    
     for instruction in instructions:
-        print(instruction)
+        crates = execute_instruction(instruction[0], instruction[1], instruction[2], crates)
+    
+    top_items = []
 
-    return ""
+    for crate in crates:
+        top_items.append(crates[crate][-1])
+
+    return "".join(top_items)
+
+def execute_instruction(move,point_from, point_to, crates):
+        removed_items = []
+
+        #minus one crate in from and append it to point to
+        for i in range(0, move):
+            if len(crates[point_from]) > 0:
+                removed_crate = crates[point_from].pop()
+                crates[point_to].append(removed_crate)
+
+        return crates
+        
+
 
 def read_file_contents(file_name):
     with open(file_name) as file:
