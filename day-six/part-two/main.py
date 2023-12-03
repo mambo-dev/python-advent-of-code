b63ccad231b39c959fa581e97b5fa531e4db35f4
mambo-dev <mambo.michael.22@gmail.com>
2023-12-03 18:36:03 +0300

    complete day six part two

--- day-six/part-two/main.py
+++ day-six/part-two/main.py
@@ -1,11 +1,12 @@
 def main():
-    example_input = read_file_contents("example-input.txt")
-    main_input = read_file_contents("main-input.txt")
+    example_input = read_file_contents("../example-input.txt")
+    main_input = read_file_contents("../main-input.txt")
 
-    position_found = start_sub_routine(main_input)
+    position_found = start_of_message(main_input)
     print("found at position", position_found)
 
-def start_sub_routine(input):
+
+def start_of_message(input):
     store_inputs = {}
     found_at  = ""
     duplicate_found_at = 0
@@ -13,15 +14,15 @@ def start_sub_routine(input):
     store_four_from_current_index = []
     for i in range(0,len(input)):
         next_idx = i + 1
-        for j in range(i , i + 4):
+        for j in range(i , i + 14):
             
             store_four_from_current_index.append(input[j])
-            if len(store_four_from_current_index) == 4:
+            if len(store_four_from_current_index) == 14:
                 if is_unique(store_four_from_current_index):
                     first_marker = store_four_from_current_index[-1]
                     store_four_from_current_index.clear()
                    
-                    return i + 4
+                    return i + 14
                 else:
             
                     next_idx, i =  i, next_idx
@@ -31,7 +32,7 @@ def start_sub_routine(input):
     
           
 
-    return first_marker
+    return first_message_marker
 
 def is_unique(lst):
     return len(lst) == len(set(lst))
