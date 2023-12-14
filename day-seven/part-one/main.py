defff22c859f34d90b4399190cd1a3860ea541d7
mambo-dev <mambo.michael.22@gmail.com>
2023-12-14 23:08:45 +0300

    first trial at day one

--- day-seven/part-one/main.py
+++ day-seven/part-one/main.py
@@ -1,17 +1,37 @@
 def main():
     print("=== advent of code day seven ===")
     main_input = read_file_contents("main-input.txt")
-    example_input = read_file_contents("example-input")
+    example_input = read_file_contents("example-input.txt")
     largest_directories  = start_read_process(example_input) 
-    print(largest_directories)
 
-def start_read_process():
+
+def start_read_process(input):
     largest_directories = []
+    split_input = input.split("\n")
+    directories = {}
+    current_directory = ""
+    for i in range(0, len(split_input)):
+        line = split_input[i]
+        if line.startswith("$"):
+            instruction = line.split()[1]
+            if instruction == "cd":
+                directory = line.split()[2]
+                current_directory = directory
+
+          
+
+                    
+               
+    print("this is directories tree", directories)
+        
 
     return largest_directories
 
 def read_file_contents(file_name):
-    with open(file_name) as file:
-        return file.read()
+    try:
+        with open(file_name) as file:
+            return file.read()
+    except FileNotFoundError:
+        raise Exception("The file you are trying to open was not found")
 
 main()
