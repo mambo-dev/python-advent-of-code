ba9295586a2f9c12222c5b5b456edc2807bb283c
mambo-dev <mambo.michael.22@gmail.com>
2024-03-05 21:50:29 +0300

    just realized tail goes where the head goesgit add .oops

--- day-nine/part-one/main.py
+++ day-nine/part-one/main.py
@@ -6,39 +6,64 @@ def main():
 
 
 def start_simulation(input):
+    visited = 0
 
     current_positions = {
-        "head_position":0,
-        "tail_position":0,
+        "head_positions":{
+            "U":0,
+            "R":0,
+            "D":0,
+            "L":0
+        },
+        "tail_positions":{
+            "U":0,
+            "R":0,
+            "D":0,
+            "L":0
+        },
         "start_position":0
     }
 
     for input in input.split("\n"):
-        if input[0] == "R":
-           current_positions = move_right(current_positions, int(input[2]))
-        if input[0] == "U":
-            current_positions = move_up(current_positions, int(input[2]))
+        steps = int(input[2])
+        direction = input[0]
+        
+        if direction == "R":
+           current_positions, visited = move_right(current_positions, steps , visited)
+           print(visited)
+
+        if direction == "U":
+            current_positions, visited = move_up(current_positions, steps, visited)
+            print(visited)
+    
+    print(current_positions, visited)
 
     #position name start, headPosition , tailPosition if head moves position to 1 then tail is 1 - 1, move to two tail is 2 -1
     pass
 
-def move_right(positions, steps):
+def move_right(positions, steps, visited):
     for i in range(0, steps):
-        positions["head_position"] += 1
-        difference = positions["head_position"] - positions["tail_position"]
+        positions["head_positions"]["R"] += 1
+        difference = positions["head_positions"]["R"] - positions["tail_positions"]["R"]
         if difference > 1:
-            positions["tail_position"] += 1
+            visited += 1 
+            positions["tail_positions"]["R"] += 1
     
-    return positions
+    return positions, visited
 
-def move_up(positions, steps):
+def move_up(positions, steps, visited):
     for i in range(0, steps):
-        positions["head_position"] += 1
-        difference = positions["head_position"] - positions["tail_position"]
+        if positions["head_positions"]["U"] == 0:
+            positions["head_positions"]["L"] += 1
+        positions["head_positions"]["U"] += 1
+        
+        difference = positions["head_positions"]["U"] - positions["tail_positions"]["U"]
+
         if difference > 1:
-            positions["tail_position"] += 1
+            visited += 1 
+            positions["tail_positions"]["U"] += 1
     
-    return positions
+    return positions, visited
 
 
 
