d875b93737265cb59e9859ff8df2655728143d0e
mambo-dev <mambo.michael.22@gmail.com>
2024-05-09 07:10:36 +0300

    feat: day nine rebuild

--- day-nine/part-one/main.py
+++ day-nine/part-one/main.py
@@ -1,3 +1,5 @@
+import functools
+
 def main():
     print("=== advent of code day nine ===")
     main_input = read_file_contents("main-input.txt")
@@ -7,63 +9,70 @@ def main():
 
 def start_simulation(input):
     visited = 0
-
-    current_positions = {
-        "head_positions":{
-            "U":0,
-            "R":0,
-            "D":0,
-            "L":0
-        },
-        "tail_positions":{
-            "U":0,
-            "R":0,
-            "D":0,
-            "L":0
-        },
-        "start_position":0
+    highest_positions = {
+        "left":0,
+        "right":0,
+        "up":0,
+        "down":0
     }
+    movements = input.split("\n")
 
-    for input in input.split("\n"):
-        steps = int(input[2])
-        direction = input[0]
-        
-        if direction == "R":
-           current_positions, visited = move_right(current_positions, steps , visited)
-           print(visited)
+    for input in movements:
+        step = int(input[2])
+        match input[0]:
+            case "L":
+                if(highest_positions["left"] < step):
+                    highest_positions["left"] = step
+            case "R":
+                if(highest_positions["right"] < step):
+                    highest_positions["right"] = step
+            case "U":
+                if(highest_positions["up"] < step):
+                    highest_positions["up"] = step
+            case "D":
+                if(highest_positions["down"] < step):
+                    highest_positions["down"] = step
+            case _:
+                print("something went wrong")
+            
+    columns = highest_positions["left"] + highest_positions["right"]
+    rows = highest_positions["up"] + highest_positions["down"]
+    knot_positions = [[{"visited":False}]*columns ] * rows
+    for i in range(0, len(movements) - 1):
+        steps = movements[i][2]
+        direction = movements[i][0]
+        for step in range(1, int(steps) + 1):
+         
+            if(direction == "R"):
+                knot_positions[len(knot_positions) - 1][step] = {"visited":True}
+            elif(direction == "U"):
+                knot_positions[len(knot_positions) -1][step]= {"visited":True}
 
-        if direction == "U":
-            current_positions, visited = move_up(current_positions, steps, visited)
-            print(visited)
-    
-    print(current_positions, visited)
+    print(knot_positions)
+           
+            
 
-    #position name start, headPosition , tailPosition if head moves position to 1 then tail is 1 - 1, move to two tail is 2 -1
+    # sposition name start, headPosition , tailPosition if head moves position to 1 then tail is 1 - 1, move to two tail is 2 -1
     pass
 
-def move_right(positions, steps, visited):
-    for i in range(0, steps):
-        positions["head_positions"]["R"] += 1
-        difference = positions["head_positions"]["R"] - positions["tail_positions"]["R"]
-        if difference > 1:
-            visited += 1 
-            positions["tail_positions"]["R"] += 1
-    
-    return positions, visited
 
-def move_up(positions, steps, visited):
+def pull_tail(steps, direction, positions):
+    visited = 0
+    saveVisits = []
     for i in range(0, steps):
-        if positions["head_positions"]["U"] == 0:
-            positions["head_positions"]["L"] += 1
-        positions["head_positions"]["U"] += 1
-        
-        difference = positions["head_positions"]["U"] - positions["tail_positions"]["U"]
+        difference = positions["head_positions"][direction] - positions["tail_positions"][direction]
+        head_position = positions["head_positions"][direction]
+        if(difference > 0):
+            positions["tail_positions"][direction] = head_position
+            visited += 1
+            
 
-        if difference > 1:
-            visited += 1 
-            positions["tail_positions"]["U"] += 1
-    
-    return positions, visited
+        positions["head_positions"][direction] += 1
+
+
+    print(visited, saveVisits)
+    return visited
+
 
 
 
